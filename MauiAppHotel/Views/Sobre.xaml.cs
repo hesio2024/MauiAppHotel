8b219264babb1a898f903c8bd5875a4923316412
hesio <hesios@gmail.com>
2024-11-16 18:39:52 -0300

    Add recursos ao app

---
+++ MauiAppHotel/Views/Sobre.xaml.cs
@@ -0,0 +1,21 @@
+namespace MauiAppHotel.Views;
+
+public partial class Sobre : ContentPage
+{
+	public Sobre()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Navigation.PushAsync(new ContratacaoHospedagem());
+
+		} catch (Exception ex)
+		{
+			DisplayAlert("Ops", ex.Message, "Ok");
+		}
+    }
+}
